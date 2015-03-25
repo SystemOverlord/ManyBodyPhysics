054a3e8a979b7b1976dac0ed4b9c5c74e32607a2
Lukas Hoermann <hoerma_l@itp.tugraz.at>
2015-03-25 15:03:58 +0100

    Habs geaendert

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,154 +1,224 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 12 10:51:20 2015
+Created on Thu Mar 19 23:40:46 2015
 
-@author: ranftl_s
+@author: lukashoermann
 """
 
 import numpy
-from collections import Counter
+from scipy.sparse import csr_matrix
 
 def nearest_neighbours(N,M):
-    
     NN = numpy.array([[0,0]]*N*M)
     
     for k in xrange(N*M):
+        
         # oberer Nachbar
-        if k < M:  NN[k][0] = (N-1)*M + k
+        if k < M: NN[k][0] = (N-1)*M + k
         else: NN[k][0] = k - M
         
         # rechter Nachbar
         if (k+1) % M == 0: NN[k][1] = k - M + 1
         else: NN[k][1] = k + 1
-    
+        
     return NN
     
-def basis(front0, n1, n2):   
-# example: n1 = 8, n2 = 3, k = 0
-# front = 00001000
-# back =  00000011
-# vec =   00001011
+def basis(front0, n1, n2, N1):
+    # example: n1 = 8, n2 = 3, k = 0
+    # front = 00001000
+    # back = 00000011
+    # spin_add = 00001011
 
     global spin_up, spin_down, spin
     
     for k in xrange(0, n1-n2):
+        front = 1<<(n2+k) #2**(n2+k)
+        back = ( 1<<(n2-1) ) - 1  #2**(n2-1)-1
         
-        front = 2**(n2+k)
-        back = 2**(n2-1)-1
+        spin_add = front0 + front + back
+        spin_up_add = spin_add>>N1 #fuer 2x2 matrix
         
-        vec = front0 + front + back
+        spin = numpy.hstack((spin, spin_add))
+        spin_up = numpy.hstack((spin_up, spin_up_add))
+        spin_down = numpy.hstack((spin_down, spin_add - (spin_up_add<<N1)))
         
-        vec1 = vec>>4 #füe 2x2 matrix
-        spin = numpy.hstack((spin, vec))
-        spin_up = numpy.hstack((spin_up, vec1))
-        spin_down = numpy.hstack((spin_down, vec - (vec1<<4)))
-        
-        if n2 > 1:# and not k == 0:
+        if n2 > 1:
             front1 = front0 + front
-            basis(front1, n2+k, n2-1)
-
-def hamilton_diag(spin_up, spin_down, spin, NN, N1):
-# N1 = N*M
+            basis(front1, n2+k, n2-1, N1)
     
+def hamilton_diag(spin_up, spin_down, spin, NN, N1):
+    # N1 = N*M
     # dictionary um die bisecs zu den k effizient zu speichern
-    flip_list={}
-
+    H = numpy.array([[0,0,0]], dtype=float)
     len_basis = len(spin_up)
-    diag = numpy.array([0]*len_basis, dtype=float)
+    # initialisiere diagonale der hamiltom matrix
+    
     for k in xrange(len_basis):
-        spin_up0 = [int(x) for x in bin(spin_up[k])[2:]]
-        spin_up0 = numpy.array([0]*(N1-len(spin_up0)) + spin_up0, dtype=float) 
-
-        spin_down0 = [int(x) for x in bin(spin_down[k])[2:]]
-        spin_down0 = numpy.array([0]*(N1-len(spin_down0)) + spin_down0, dtype=float)
         
-        spin_total = spin_up0 - spin_down0
+        # erstelle temporäres array zur speicherung der bisecs
+        # ([m, n, Betrag Matrixelement])
+        flip_tmp = numpy.array([[0,0,0]], dtype=int)
         
-        # erstelle temporäres array tzr speicherung der bisecs
-        flip_tmp=numpy.array([])
+        # initialisiere Diagonalelement
+        diag_neu0 = 0
         
         for l in xrange(N1):
-            if not spin_total[l] == 0:
-                diag[k] += ( spin_total[NN[l][0]] + spin_total[NN[l][1]]) * (spin_total[l]) * 0.25
+            mask_tot = 1<<(N1-l-1)
             
-            # Wenn die Spins von k und über k unterschiedlich
-            if spin_total[NN[l][0]]*spin_total[l] == -1:
-                # erstelle eine Maske mit 1en an den Stellen der zu Tauschenden Spins
-                mask = (1<<l) | (1<<NN[l][0])
-                # und Tausche mit dieser Maske die Spins
-                spin_up_neu = spin_up[k] ^ mask
-                spin_down_neu = spin_down[k] ^ mask
-                spin_neu = (spin_up_neu << 4)
-                spin_neu ^= spin_down_neu
-                # suche die soeben erstellte Konfiguration in dem Vector aller Konfigurationen
-                bisec = bisection(spin,spin_neu)
-                # und speichere den Index in einem Vector
-                flip_tmp = numpy.hstack((flip_tmp,bisec))
-                #print('k:',k,'tmp:',flip_tmp)
+            #if not spin_total[l] == 0:
+            if (spin_up[k] ^ spin_down[k]) & mask_tot:
+                mask1 = 1<<(N1-NN[l][0]-1)
+                mask2 = 1<<(N1-NN[l][1]-1)
+
+                S1 = bool(spin_up[k] & mask_tot) #spin_up an postion l
+                S2 = bool(spin_down[k] & mask_tot) #spin_down an postion l
                 
-            # Wenn die Spins von k und neben k unterschiedlich    
-            if spin_total[NN[l][1]]*spin_total[l] == -1:
-                # erstelle eine Maske mit 1en an den Stellen der zu Tauschenden Spins
-                mask = (1<<l) | (1<<NN[l][1])
-                 # und Tausche mit dieser Maske die Spins                
-                spin_up_neu = spin_up[k] ^ mask
-                spin_down_neu = spin_down[k] ^ mask            
-                spin_neu = (spin_up_neu << 4)
-                spin_neu ^= spin_down_neu
-                # suche die soeben erstellte Konfiguration in dem Vector aller Konfigurationen
-                bisec = bisection(spin,spin_neu)
-                # speichere den Index in einem Vector
-                flip_tmp = numpy.hstack((flip_tmp,bisec))
+                S1_NN = bool(spin_up[k] & mask1) #oberen NN von l ist spin_up
+                S2_NN = bool(spin_up[k] & mask2) #rechter NN von l ist spin_up
+                S3_NN = bool(spin_down[k] & mask1) #oberen NN von l ist spin_down
+                S4_NN = bool(spin_down[k] & mask2) #rechter NN von l ist spin_down
+                
+                # berechne Diagonalelement
+                if S1:
+                    if S1_NN: diag_neu0 += 0.25
+                    if S2_NN: diag_neu0 += 0.25
+                    if S3_NN: diag_neu0 -= 0.25
+                    if S4_NN: diag_neu0 -= 0.25
 
-        # Wenn ein Eintrag vorhanden: erstelle einen Dichtionary Eintrag k welcher das Array der bisecs referenziert        
-        if flip_tmp.size : flip_list[k] = flip_tmp
+                if S2:
+                    if S3_NN: diag_neu0 += 0.25
+                    if S4_NN: diag_neu0 += 0.25
+                    if S1_NN: diag_neu0 -= 0.25
+                    if S2_NN: diag_neu0 -= 0.25
+                
+                # Flipoperator mit Spin oberhalb
+                # 1. ob überhaupt Spin an der Position des nächsten Nachbarn
+                # 2. ob Spins an Position l und an der Position des nächsten Nachbar unterschiedlich sind
+                if (spin_up[k] ^ spin_down[k]) & mask1 and (S1_NN & S2 or S3_NN & S1):
+                    # erstelle eine Maske mit 1en an den Stellen der zu Tauschenden Spins
+                    mask = (1<<(N1-l-1)) | (1<<(N1-NN[l][0]-1))
+                    
+                    # und Tausche mit dieser Maske die Spins
+                    spin_up_neu = spin_up[k] ^ mask
+                    spin_down_neu = spin_down[k] ^ mask
+                    spin_neu = (spin_up_neu << N1)
+                    spin_neu ^= spin_down_neu
+                    
+                    # suche die soeben erstellte Konfiguration in dem Vector aller Konfigurationen
+                    bisec = bisection(spin,spin_neu)
 
-    return(diag,flip_list)
+                    # trigger um Matrixelement zu speichern
+                    index_neu = 1
+                    # such in array der Matrixelemente nach doppeltem Eintrag
+                    # bei doppeltem Eintrag addiere Matrixelemente
+                    for m in xrange(1,len(flip_tmp)):                     
+                        
+                        if bisec == flip_tmp[m][1]:
+                            flip_tmp[m][2] += 1
+                            index_neu = 0
+                            break
+                    
+                    # und speichere den Index in einem Vector
+                    if index_neu == 1:
+                        flip_tmp = numpy.vstack((flip_tmp, numpy.array([[k,bisec,1]])))
+                
+                # Flipoperator mit Spin rechts
+                # 1. ob überhaupt Spin an der Position des nächsten Nachbarn
+                # 2. ob Spins an Position l und an der Position des nächsten Nachbar unterschiedlich sindv
+                if (spin_up[k] ^ spin_down[k]) & mask2 and (S2_NN & S2 or S4_NN & S1):
+                    # erstelle eine Maske mit 1en an den Stellen der zu Tauschenden Spins
+                    mask = (1<<(N1-l-1)) | (1<<(N1-NN[l][1]-1))
+                    
+                    # und Tausche mit dieser Maske die Spins
+                    spin_up_neu = spin_up[k] ^ mask
+                    spin_down_neu = spin_down[k] ^ mask
+                    spin_neu = (spin_up_neu << N1)
+                    spin_neu ^= spin_down_neu
+                    
+                    # suche die soeben erstellte Konfiguration in dem Vector aller Konfigurationen
+                    bisec = bisection(spin,spin_neu)
 
+                    # trigger um Matrixelement zu speichern
+                    index_neu = 1
+                    # such in array der Matrixelemente nach doppeltem Eintrag
+                    # bei doppeltem Eintrag addiere Matrixelemente
+                    for m in xrange(1,len(flip_tmp)):                     
+                        
+                        if bisec == flip_tmp[m][1]:
+                            flip_tmp[m][2] += 1
+                            index_neu = 0
+                            break
+                    
+                    # und speichere den Index in einem Vector
+                    if index_neu == 1:
+                        flip_tmp = numpy.vstack((flip_tmp, numpy.array([[k,bisec,1]])))
+        
+        # speichere Diagonalelement wenn ungleich 0
+        if not diag_neu0 == 0:
+            diag_neu = numpy.array([k,k, diag_neu0])
+            H = numpy.vstack((H, diag_neu))
+              
+        # Wenn ein Eintrag vorhanden: schpeichere flip_tem in flip_list
+        if flip_tmp.size-1: H = numpy.vstack((H, flip_tmp[1::]))
+        
+    return H[1::] #diag
+    
 def bisection(spin, tofind):
     count = 1
-    break_while = 2
+    break_while = 6
     index = (len(spin)>>1)
-
+    
     while break_while:
+        
         if tofind == spin[index]:
             return(index)
+
         elif tofind < spin[index]:
             count += 1
             index_shift = len(spin)>>(count)
+            
             if index_shift == 0:
                 break_while -= 1
                 index -= 1
             else:
                 index -= index_shift
-        else: 
+        else:
             count += 1
             index_shift = len(spin)>>(count)
+            
             if index_shift == 0:
                 break_while -= 1
                 index += 1
             else:
                 index += index_shift
-            
+        
     return(None)
-    
 
-N = 2 # matrix zeile
-M = 2# matrix spalte
-N2 = 4 #total number of particles with spin up
+
+# Main
+
+N = 3 # matrix zeile
+M = 3 # matrix spalte
+N2 = 3 #total number of particles with spin up
 
 spin = numpy.array([2**N2-1], dtype=int)
 spin_up = numpy.array([0], dtype=int)
 spin_down = numpy.array([2**N2-1], dtype=int)
 
-basis(0, 2*N*M, N2)
+# berechne alle Basiszustände
+basis(0, 2*N*M, N2, N*M)
 
+# berechne Indizes der Nächste Nachbarn
 NN = nearest_neighbours(N,M)
 
-diag,flip = hamilton_diag(spin_up, spin_down, spin, NN, N*M)
+# berechne Matrix des Heisenberg Hamilton Operators
+H = hamilton_diag(spin_up, spin_down, spin, NN, N*M)
 
-print(diag)
-print('\n\n')
-for x in flip:
-    print('k:',x,'bisecs:',numpy.sort(flip[x]),'counter:',Counter(flip[x]))  #nun ein dictionary, Zugriff mit flip[k] = numpy.array
+# transformiere H in eine sparse Matrix
+H = csr_matrix(( H[:,2], (H[:,0], H[:,1]) ), shape=(len(spin), len(spin)))
+
+#print(diag)
+#print('\n\n')
+print(H) #nun ein dictionary, Zugriff mit flip[k] = numpy.array
+
 
