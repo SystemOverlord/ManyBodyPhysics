1e7a5da40b73e4d8722b45a05cfe825df5b9a89d
Lukas Hoermann <hoerma_l@itp.tugraz.at>
2015-07-08 17:03:56 +0200

    bla

--- weltl_loop_v4.py
+++ weltl_loop_v4.py
@@ -11,23 +11,6 @@ import random
 import time
 from scipy import weave
 
-class walk_class:
-    def __init__(self, walk_num):
-        self.walk = walk_num
-    
-        if self.walk == 1: #rechts
-            self.x = -0.5
-            self.y = 0.5
-        elif self.walk == 2: #unten
-            self.x = 0.5
-            self.y = 0.5
-        elif self.walk == 3: #links
-            self.x = 0.5
-            self.y = -0.5
-        elif self.walk == 4: #oben
-            self.x = -0.5
-            self.y = -0.5    
-
 def Startconf(anzTeilchen,anzSpinUp,anzZeitschritte):
     
     weltlinien = np.array([[False]*anzTeilchen]*anzZeitschritte)    
@@ -164,13 +147,12 @@ def loop1(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien):
         if weltlinienschnitt > 2 and spinerhaltung: break
             
     return x,y,l,k
-       
-def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, beta):
-    
-    weltlinienschnitt = 0  
-    spinerhaltung = False
-    update = True
 
+def walk_neu(anzZeitschritte, anzTeilchen, weltlinien, x, y, k, walkOld, gs, gd):
+
+    #w = np.tanh(Jz*beta/anzZeitschritte)
+    #w = np.exp(beta * (gs_ratio - gd_ratio) )
+    
     mask1 = np.array([[0,0],[0,0]])
     mask2 = np.array([[1,1],[1,1]])
     mask3 = np.array([[1,0],[1,0]])
@@ -181,8 +163,51 @@ def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, 
     walk_h = np.array([2, 1, 4, 3])
     walk_v = np.array([4, 3, 2, 1])
     
-    w = np.tanh(Jz*beta/anzZeitschritte)
+    x_u = x[k-1]+0.5
+    x_o = x[k-1]-0.5
+    y_r = y[k-1]+0.5
+    y_l = y[k-1]-0.5
+                
+    # Periodische Randbedingungen        
+    if x_u == anzZeitschritte: x_u = 0
+    if x_o == -1: x_o = anzZeitschritte-1
+    if y_r == anzTeilchen: y_r = 0
+    if y_l == -1: y_l = anzTeilchen-1
+            
+    links_unten = weltlinien[x_u, y_l]
+    rechts_unten = weltlinien[x_u, y_r]
+    links_oben = weltlinien[x_o, y_l]
+    rechts_oben = weltlinien[x_o, y_r]
+                
+    plakette = np.array([[links_oben, rechts_oben],[links_unten,rechts_unten]])
+                
+    # Plaketten vom Typ 1+, 1- (siehe Everz S9)
+    if np.array_equal(plakette, mask1) or np.array_equal(plakette, mask2):
+        if random.random() < gs / (gs + gd): walk = walk_v[walkOld-1]
+        else: walk = walkOld
+                
+    # Plaketten vom Typ 2+, 2- (siehe Everz S9)
+    elif np.array_equal(plakette, mask3) or np.array_equal(plakette, mask4):
+        if random.random() < 1./(1.+gd): walk = walk_v[walkOld-1]
+        else: walk = walk_h[walkOld-1]
+                
+    # Plaketten vom Typ 3+, 3- (siehe Everz S9)
+    elif np.array_equal(plakette, mask5) or np.array_equal(plakette, mask6):
+        if random.random() < 1./(1.+gs): walk = walkOld
+        else: walk = walk_h[walkOld-1]
+                    
+    else:
+        print 'Plakette verboten'
+        exit(1)
+        
+    return walk
+       
+def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, beta, gs, gd):
     
+    weltlinienschnitt = 0  
+    spinerhaltung = False
+    update = True
+        
     for m in xrange(100):
         
         x = np.array([0.0]*termination)              # Initialisierung
@@ -190,7 +215,7 @@ def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, 
         
         x[0] = random.randint(0,anzZeitschritte-1)
         y[0] = random.randint(0,anzTeilchen-1)  
-        while weltlinien[x[0],y[0]] == 0:
+        while weltlinien[x[0],y[0]] == False:
             x[0] = random.randint(0,anzZeitschritte-1)
             y[0] = random.randint(0,anzTeilchen-1)
             
@@ -199,44 +224,13 @@ def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, 
         if x[0]%2 == y[0]%2: walk = 1
         else: walk = 4
         
-        walkOld = walk
-    
         for k in xrange(1,termination):
             
             if k%2 == 0:  # Jeder zweite Schritt geht doppelt
-                x_u = x[k-1]+0.5
-                x_o = x[k-1]-0.5
-                y_r = y[k-1]+0.5
-                y_l = y[k-1]-0.5
-                
-                # Periodische Randbedingungen        
-                if x_u == anzZeitschritte: x_u = 0
-                if x_o == -1: x_o = anzZeitschritte-1
-                if y_r == anzTeilchen: y_r = 0
-                if y_l == -1: y_l = anzTeilchen-1
-            
-                links_unten = weltlinien[x_u, y_l]
-                rechts_unten = weltlinien[x_u, y_r]
-                links_oben = weltlinien[x_o, y_l]
-                rechts_oben = weltlinien[x_o, y_r]
-                
-                plakette = np.array([[links_oben, rechts_oben],[links_unten,rechts_unten]])
-                
-                if np.array_equal(plakette, mask1) or np.array_equal(plakette, mask2):
-                    walk = walk_v[walkOld-1]
-                    
-                elif np.array_equal(plakette, mask3) or np.array_equal(plakette, mask4):
-                    if w < random.random(): walk = walk_h[walkOld-1]
-                    else: walk = walk_v[walkOld-1]
-                    
-                elif np.array_equal(plakette, mask5) or np.array_equal(plakette, mask6):
-                    walk = walk_h[walkOld-1]
-                    
-                else: print 'Plakette verboten'
-                
-                walkOld = walk
+                walk = walk_neu(anzZeitschritte, anzTeilchen, weltlinien, x, y, k, walk, gs, gd)
             
             # switch für Schritt
+            
             if walk == 1: #rechts
                 x[k] = x[k-1]-0.5
                 y[k] = y[k-1]+0.5
@@ -246,18 +240,21 @@ def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, 
             elif walk == 3: #links
                 x[k] = x[k-1]+0.5
                 y[k] = y[k-1]-0.5
-            elif walk == 4: #oben
+            #elif walk == 4: #oben
+            else:
                 x[k] = x[k-1]-0.5
                 y[k] = y[k-1]-0.5
             
+            #TODO: optimieren: class xy
             # Periodische Randbedingungen        
             if x[k] == anzZeitschritte: x[k] = 0
             if x[k] == -1: x[k] = anzZeitschritte-1
             if y[k] == anzTeilchen: y[k] = 0
             if y[k] == -1: y[k] = anzTeilchen-1
             
+            #TODO: sort + bisec 
             # Suche ob Loop sich beißt
-            for l in xrange(0,k):
+            for l in xrange(0,k-1):
                     
                 if (x[l]== x[k]) & (y[l] == y[k]):
                     x = x[l:k]
@@ -278,7 +275,10 @@ def loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, 
                 spinerhaltung_mask = np.array([False]*anzTeilchen)
                 weltlinienschnitt = 0
                 for n in xrange(len(x)):
+                    # sucht nach Loop-Schnittpunkten in der ersten Zeile (x[n] == 0) des Schachbretts
                     if x[n] == 0: spinerhaltung_mask[y[n]] = True
+                    
+                    # sucht Schnittpunkte mit Weltlinien
                     if weltlinien[x[n],y[n]] == True: weltlinienschnitt += 1
                 
                 spinerhaltung = False
@@ -304,10 +304,14 @@ def gewichter2(weltlinien):
     
     ns_py = np.array([0])
     nd_py = np.array([0])
+    n0_py = np.array([0])
+    val_py= np.array([0])
     
     code = r'''
     int ns = 0;
     int nd = 0;
+    int n0 = 0;
+    int val = 0;
     
     for(int n = 0; n < N-1; n++) {
         for(int m = 0; m < M-1; m++) {
@@ -315,40 +319,45 @@ def gewichter2(weltlinien):
             if (n%2 != m%2) {
                 int index1 = (n+1)*M + m;
                 int index2 = (n+1)*M + m + 1;
-                int index3 = (n)*M + m;
-                int index4 = (n)*M + m + 1;
+                int index3 = n*M + m;
+                int index4 = n*M + m + 1;
                 
                 int links_unten = weltlinien[index1];
                 int rechts_unten = weltlinien[index2];
                 int links_oben = weltlinien[index3];
                 int rechts_oben = weltlinien[index4];
                 
-                if(links_unten == 1 & rechts_unten == 0 & links_oben == 1 & rechts_oben == 0) {
+                int valide = 0;
+                if(links_unten == links_oben && rechts_oben == rechts_unten && rechts_unten != links_oben) {
                     ns += 1;
+                    valide +=1;
                     }
-                
-                else if(links_unten == 0 & rechts_unten == 1 & links_oben == 0 & rechts_oben == 1) {
-                    ns += 1;
-                    }
-            
-                else if(links_unten == 1 & rechts_unten == 0 & links_oben == 0 & rechts_oben == 1) {
+                if(links_unten != links_oben && rechts_oben != rechts_unten && rechts_unten == links_oben) {
                     nd += 1;
+                    valide +=1;
                     }
+                if(links_unten == links_oben && rechts_oben == rechts_unten && rechts_unten == links_oben) {
+                    n0 += 1;
+                    valide +=1;
+                    }
+                if(valide!=1){val+=1;}
                     
-                else if(links_unten == 0 & rechts_unten == 1 & links_oben == 1 & rechts_oben == 0) {
-                    nd += 1;
-                    }
             }
         }    
     }
     ns_py[0] = ns;
     nd_py[0] = nd;
+    n0_py[0] = n0;
+    val_py[0] = val;
 
     '''
 
-    weave.inline(code,['N','M','ns_py','nd_py','weltlinien'])
+    weave.inline(code,['N','M','ns_py','nd_py','n0_py','val_py','weltlinien'])
     
-    return ns_py, nd_py
+    if val_py !=0:
+        print val_py
+        exit(1)
+    return ns_py, nd_py, n0_py
 
 
 def autocorr(x):
@@ -362,67 +371,71 @@ def autocorr(x):
 
 t0 = time.time()
 # Parameter
-anzTeilchen = 12
-anzSpinup = 6
-anzZeitschritte = 100
+anzTeilchen = 16
+anzSpinup = 8
+anzZeitschritte = 200
 termination = anzTeilchen*anzZeitschritte
 anzMarkovZeit = 10000
-#m = anzTeilchen
 
 Jz = 1.
 Jx = 1.
-beta = 0.01
+beta = 20
 
-#gs = np.exp(-Jz*beta/(2*anzZeitschritte)) * np.cosh(Jx*beta/anzZeitschritte)
-#gd = np.exp(-Jz*beta/(2*anzZeitschritte)) * np.sinh(Jx*beta/anzZeitschritte)
-gs_ratio = Jz/(anzZeitschritte) * (np.tanh(beta*Jx/anzZeitschritte) - 1) #gs' / gs
-gd_ratio = Jz/(anzZeitschritte) * (1/np.tanh(beta*Jx/anzZeitschritte) - 1) #gd' / gd
+gs = np.exp(-Jz*beta/anzZeitschritte) * np.cosh(Jx*beta/anzZeitschritte)
+gd = np.exp(-Jz*beta/anzZeitschritte) * np.sinh(Jx*beta/anzZeitschritte)
+gs_ratio = Jx/anzZeitschritte * np.tanh(beta*Jx/anzZeitschritte) - Jz/anzZeitschritte #gs' / gs
+gd_ratio = Jx/anzZeitschritte / np.tanh(beta*Jx/anzZeitschritte) - Jz/anzZeitschritte #gd' / gd
 
-meanNs = np.array([0]*anzMarkovZeit)
-meanNd = np.array([0]*anzMarkovZeit)
-energy = np.array([0.]*anzMarkovZeit)
+meanNs = np.array([0]*(anzMarkovZeit-1))
+meanNd = np.array([0]*(anzMarkovZeit-1))
+meanN0 = np.array([0]*(anzMarkovZeit-1))
+energy = np.array([0.]*(anzMarkovZeit-1))
 
 # Start
 weltlinien = Startconf(anzTeilchen,anzSpinup,anzZeitschritte)
 
-meanNs[0] = anzTeilchen
-meanNd[0] = 0
-energy[0] = - meanNs[0]*Jz/(anzZeitschritte) * gs_ratio
-
-# heat up array
-for m in xrange(20):
-    
-    # Loop finden
-    x,y,l,k = loop1(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien)
-
-    # Spinflip
-    for k in xrange(len(x)):
-        weltlinien[int(x[k]),int(y[k])] = weltlinien[int(x[k]),int(y[k])] ^ True
+## heat up array
+#for m in xrange(20):
+#    
+#    # Loop finden
+#    x,y,l,k = loop1(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien)
+#
+#    # Spinflip
+#    for k in xrange(len(x)):
+#        weltlinien[int(x[k]),int(y[k])] = weltlinien[int(x[k]),int(y[k])] ^ True
 
 # run simulation
-for n in xrange(1,anzMarkovZeit):
+for n in xrange(anzMarkovZeit-1):
     print n    
     
     # Loop finden
-    x,y,l,k,update = loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, beta)
+    x,y,l,k,update = loop2(anzZeitschritte, anzTeilchen, anzSpinup, termination, weltlinien, Jz, beta, gs, gd)
 
     # Spinflip
-    if update:
+    if update and 0.5 > random.random():
         for k in xrange(len(x)):
-            weltlinien[int(x[k]),int(y[k])] = weltlinien[int(x[k]),int(y[k])] ^ True 
+            weltlinien[int(x[k]),int(y[k])] = weltlinien[int(x[k]),int(y[k])] ^ True
         
-        # Gewichter der Weltlinienkonfiguration berechnen   
-        ns, nd = gewichter2(weltlinien)
+    # Gewichter der Weltlinienkonfiguration berechnen   
+    ns, nd, n0 = gewichter2(weltlinien)
         
-        test_energy = -ns*gs_ratio - nd*gd_ratio
-        test_energy /= (anzTeilchen*anzZeitschritte)
+    test_energy = -ns*gs_ratio - nd*gd_ratio
+    test_energy /= anzTeilchen
         
-        meanNs[n] = int(ns)
-        meanNd[n] = int(nd)
-        energy[n] = test_energy
+    meanNs[n] = int(ns)
+    meanNd[n] = int(nd)
+    meanN0[n] = int(n0)
+    energy[n] = test_energy - Jz/4
         
 
-print(np.mean(energy[1000::]) - Jz/4)
+mean_ns = np.mean(meanNs)
+mean_nd = np.mean(meanNd)
+mean_E = -mean_ns*gs_ratio - mean_nd*gd_ratio
+mean_E /= anzTeilchen
+mean_E -= Jz/4
+
+print('E_mean', mean_E)
+#print(np.mean(energy[1000::]) - Jz/4)
 #print(t0 - time.time())
 
 #auto = autocorr(energy)
@@ -436,3 +449,11 @@ figEnergy = plt.figure()
 plt.plot(energy)
 plt.ylabel('Energy')
 plt.xlabel('Markov Time')
+
+figPlaketten = plt.figure()
+plt.plot(meanN0, '-k', label='S1')
+plt.plot(meanNs, '-b', label='S2')
+plt.plot(meanNd, '-r', label='S3')
+plt.ylabel('Anz Plaketten')
+plt.xlabel('Markov Time')
+plt.legend()
